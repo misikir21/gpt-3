d7e8c1c79b2ce2003a9183e78c27aa93b3f78cd1
misikira <misikirteka@gmail.com>
2023-09-10 13:45:55 +0300

    add css to my project header

--- gpt_3/src/App.js
+++ gpt_3/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import {Footer,Blog,Header,Possible,Whatgpt, Features} from './containers'
 import {Article,Brand,Cta,Feature,Navbar} from './components'
+import './App.css'
 function App() {
   return (
     <div className='app'>
-        <div className='gradient-bg'>
+        <div className='gradient__bg'>
          <Navbar />
          <Header/>    
         </div>

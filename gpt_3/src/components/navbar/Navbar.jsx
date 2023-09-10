abc82b2dd61e051b8ec26c55ed23ef8ead3ee9d6
misikira <misikirteka@gmail.com>
2023-09-10 15:13:01 +0300

    fix flex issues

--- gpt_3/src/components/navbar/Navbar.jsx
+++ gpt_3/src/components/navbar/Navbar.jsx
@@ -11,15 +11,13 @@ function Navbar() {
           <img src={logo} alt='gpt-3 logo' />
 
     </div>
-    <div className="gpt3__navbar-links_container">
-          <div className=".gpt3__navbar-links_container">
+     <div className="gpt3__navbar-links_container">
             <p><a href="#home">Home</a></p>
             <p><a href="#wgpt3">What is GPT3?</a></p>
             <p><a href="#possibility">Open AI</a></p>
             <p><a href="#features">Case Studies</a></p>
             <p><a href="#blog">Library</a></p>
           </div>
-    </div>
     </div>
     </div>
   )

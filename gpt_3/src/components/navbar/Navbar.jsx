74be2412b4edc945d2ec0c9205b380abc3e75055
misikira <misikirteka@gmail.com>
2023-09-10 14:04:33 +0300

    add assets to my project

--- gpt_3/src/components/navbar/Navbar.jsx
+++ gpt_3/src/components/navbar/Navbar.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='gpt3-navbar'>
+      <div className='gpt3-navbar_links'>
+        <div className='gpt3-navbar_links_logo'>
+          <img src='logo' alt='gpt-3 logo' />
+
+    </div>
+    </div>
+    </div>
   )
 }
 
-export default Navbar
+export default Navbar;

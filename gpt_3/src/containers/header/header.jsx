9dc374e86da55e77b7adf7a3bad860c4260c765d
misikira <misikirteka@gmail.com>
2023-09-15 16:50:17 +0300

    add header compo

--- gpt_3/src/containers/header/header.jsx
+++ gpt_3/src/containers/header/header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './header.css'
 import people from '../../Assets/people.png';
 import ai from '../../Assets/ai.png';
-const Header = () => (
+const Header = () => {return (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
       <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
@@ -24,5 +24,5 @@ const Header = () => (
     </div>
   </div>
 );
-
+}
 export default Header;

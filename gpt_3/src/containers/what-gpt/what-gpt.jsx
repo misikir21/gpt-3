62c4118c9d853cec48abd87b8dcbeb8f74e607c1
misikira <misikirteka@gmail.com>
2023-09-18 14:43:03 +0300

    add divs

--- gpt_3/src/containers/what-gpt/what-gpt.jsx
+++ gpt_3/src/containers/what-gpt/what-gpt.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import './what.css'
 function Whatgpt() {
   return (
-    <div>blog</div>
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+    <div className="gpt3__whatgpt3-feature">
+      <feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+    </div>
+    </div>
   )
 }
 

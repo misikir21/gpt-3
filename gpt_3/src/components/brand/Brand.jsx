f39f12dafc500ff2f06c23da585aadbe0a453f68
misikira <misikirteka@gmail.com>
2023-09-18 14:05:40 +0300

    add logo cotaier

--- gpt_3/src/components/brand/Brand.jsx
+++ gpt_3/src/components/brand/Brand.jsx
@@ -4,7 +4,30 @@ import { google,slack,shopify,dropbox,atlassian } from './import'
 
 function Brand() {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={google} alt="google logo" />
+      </div>
+
+      <div>
+        <img src={slack} alt="slack logo" />
+      </div>
+
+
+      <div>
+        <img src={shopify} alt="shopify logo" />
+      </div>
+
+      <div>
+        <img src={atlassian} alt="atlassia logo" />
+      </div>
+
+      <div>
+        <img src={dropbox} alt="dropox logot" />
+      </div>
+
+      
+    </div>
   )
 }
 

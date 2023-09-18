6c9099520e552deefa364a4d22037a3c63d051ba
misikira <misikirteka@gmail.com>
2023-09-18 14:44:02 +0300

    update what is gpt comp

--- gpt_3/src/containers/what-gpt/what-gpt.jsx
+++ gpt_3/src/containers/what-gpt/what-gpt.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import './what.css'
-function Whatgpt() {
-  return (
-    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+const WhatGPT3 = () => (
+  <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
     <div className="gpt3__whatgpt3-feature">
       <feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
     </div>
+    <div className="gpt3__whatgpt3-heading">
+      <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+      <p>Explore the Library</p>
     </div>
-  )
-}
+    <div className="gpt3__whatgpt3-container">
+      <feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
+      <feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+      <feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+    </div>
+  </div>
+);
 
-export default Whatgpt
+export default WhatGPT3;

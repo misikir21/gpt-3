6e1264ee0fc64d70e7ce957cd8a77631ad719ca3
misikira <misikirteka@gmail.com>
2023-09-18 15:03:21 +0300

    add comp feature

--- gpt_3/src/components/feature/Feature.jsx
+++ gpt_3/src/components/feature/Feature.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-function Feature() {
+const Feature =({title,text})=> {
   return (
-    <div>Feature</div>
+    <div className='gpt3__features section__padding" id="features"'>
+      <div className='gpt3__features-heading'>
+
+      </div>
+      <h1>{title}</h1>
+    <div className='gradient__features-container_feature_text'>
+         {text}
+    </div>
+    </div>
   )
 }
 

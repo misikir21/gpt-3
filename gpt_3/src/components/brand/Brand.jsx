2136940abd96d6f202bdde600d55db388ae0e840
misikira <misikirteka@gmail.com>
2023-10-09 14:15:37 +0300

    add alt text to images

--- gpt_3/src/components/brand/Brand.jsx
+++ gpt_3/src/components/brand/Brand.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { google, slack, atlassian, dropbox, shopify } from './imports';
-import './brand.css';
+import React from "react";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
+import "./brand.css";
 
 const Brand = () => (
-  <div className="gpt3__brand section__padding">
-    <div>
-      <img src={google} />
-    </div>
-    <div>
-      <img src={slack} />
-    </div>
-    <div>
-      <img src={atlassian} />
-    </div>
-    <div>
-      <img src={dropbox} />
-    </div>
-    <div>
-      <img src={shopify} />
-    </div>
-  </div>
+	<div className="gpt3__brand section__padding">
+		<div>
+			<img src={google} alt="google logo" />
+		</div>
+		<div>
+			<img src={slack} alt="slack logo" />
+		</div>
+		<div>
+			<img src={atlassian} alt="atliassin logo" />
+		</div>
+		<div>
+			<img src={dropbox} alt="dropbox logo" />
+		</div>
+		<div>
+			<img src={shopify} alt="shopify logo" />
+		</div>
+	</div>
 );
 
 export default Brand;
